50ec003e321043998a3779eed3575d4cc3a59cb5
Anil Chavda <anil.sammrat@gmail.com>
2020-04-13 23:12:01 +0530

    fixed issue

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -31,6 +31,7 @@ export class MemberMessagesComponent implements OnInit {
       .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
       .pipe(
         tap((messages) => {
+          // tslint:disable-next-line: prefer-for-of
           for (let i = 0; i < messages.length; i++) {
             if (
               messages[i].isRead === false &&
