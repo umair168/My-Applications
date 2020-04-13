906cee76ca91113d52b341085d4e10cab8ff59f6
Anil Chavda <anil.sammrat@gmail.com>
2020-04-14 04:45:09 +0530

    uploading final build

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -47,13 +47,13 @@ namespace DatingApp.API.Data
 
         public async Task<User> GetUser(int id)
         {
-            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
             return user;
         }
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =  _context.Users.Include(p => p.Photos)
+            var users =  _context.Users
                 .OrderByDescending(u=>u.LastActive).AsQueryable();
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
@@ -91,7 +91,7 @@ namespace DatingApp.API.Data
         private async Task<IEnumerable<int>> GetUserLikes(int id,bool likers)
         {
             var user = await _context.Users
-                .Include(x => x.Likers).Include(a => a.Likees).FirstOrDefaultAsync(a=>a.Id==id);
+              .FirstOrDefaultAsync(a=>a.Id==id);
             if (likers)
             {
                 return user.Likers.Where(u => u.LikeeId == id).Select(a=>a.LikerId);
@@ -114,9 +114,7 @@ namespace DatingApp.API.Data
 
         public async Task<PagedList<Message>> GetMessagesForUsers(MessageParams messageParams)
         {
-            var messages = _context.Messages.Include(u => u.Sender)
-                 .ThenInclude(p => p.Photos)
-                 .Include(u => u.Recipient).ThenInclude(p => p.Photos).AsQueryable();
+            var messages = _context.Messages.AsQueryable();
             switch (messageParams.MessageContainer)
             {
                 case "Inbox":
@@ -138,9 +136,7 @@ namespace DatingApp.API.Data
 
         public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
-            var messages = await _context.Messages.Include(u => u.Sender)
-              .ThenInclude(p => p.Photos)
-              .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            var messages = await _context.Messages
               .Where(m => m.RecipientId == userId && m.RecipientDeleted==false 
               && m.SenderId == recipientId || m.RecipientId == recipientId
               && m.SenderId == userId && m.SenderDeleted==false)
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -35,12 +35,31 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => {
+                x.UseLazyLoadingProxies();
+                x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+
+        });
+            ConfigureServices(services);
+        }
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            //services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => {
+                x.UseLazyLoadingProxies();
+                x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+
+            });
+            ConfigureServices(services);
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
             .ConfigureApiBehaviorOptions(o => { o.SuppressModelStateInvalidFilter = true; });
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+           // services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers().AddNewtonsoftJson(options =>
                  options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
             services.AddCors();
@@ -93,13 +112,18 @@ namespace DatingApp.API
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseAuthorization();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Values}/{action=GetValues}/{id?}");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
+
+
         }
     }
 }
